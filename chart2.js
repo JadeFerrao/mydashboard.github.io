e4dd69110f9964b560e904bbb971da45ee99d96b
Jayden Ferrao <jadeanthony0206@gmail.com>
2023-08-16 20:38:07 +0530

    updated

--- chart2.js
+++ chart2.js
@@ -1,23 +1,25 @@
-var ctx = document.getElementById('doughnut').getContext('2d');
+var ctx = document.getElementById('pieChart').getContext('2d'); // Change the context ID
 var myChart = new Chart(ctx, {
-    type: 'pie',
+    type: 'pie', // Change the chart type to 'pie'
     data: {
-        labels: ['open', 'search', 'close'],
-        datasets: [{
-            label: 'Statistics',
-            data: [12, 29, 40],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(0, 255, 0, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(0, 255, 0, 1)'
-            ],
-            borderWidth: 1
-        }]
+        labels: ['Open', 'Search', 'Close'], // Labels for each dataset
+        datasets: [
+            {
+                label: 'Data',
+                data: [3, 8, 10], // Adjust the data values accordingly
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(0, 255, 0, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(0, 255, 0, 1)'
+                ],
+                borderWidth: 1
+            }
+        ]
     },
     options: {
         responsive: false
